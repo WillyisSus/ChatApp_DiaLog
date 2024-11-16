0a7148651f6d1862587e82becd00c05fe98ae577
WillyisSus <vovietlong0845927889@gmail.com>
2024-11-16 23:29:28 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module application.chatapp_dialog {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens application.chatapp_dialog to javafx.fxml;
+    exports application.chatapp_dialog;
+}
