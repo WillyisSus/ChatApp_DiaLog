41321242e6699e06cbf62eee26c6fddfd4a251a5
WillyisSus <vovietlong0845927889@gmail.com>
2024-11-30 21:21:34 +0700

    Upload create table scripts

---
+++ database/create_tables.sql
@@ -0,0 +1,77 @@
+create database dialog_db;
+
+\connect dialog_db
+
+create table admin_accounts(
+	id serial primary key,
+	username varchar(50) not null,
+	password varchar(50) not null,
+	email varchar(200) not null,
+	displayname varchar(50),
+	create_date timestamp default current_timestamp,
+	is_online boolean default false
+);
+
+create table user_accounts(
+	id serial primary key,
+	username varchar(50) not null,
+	password varchar(50) not null,
+	email varchar(200) not null,
+	displayname varchar(50),
+	sex boolean not null,
+	address varchar(200),
+	create_date timestamp default current_timestamp,
+	is_online boolean default false,
+	is_lock boolean default false
+);
+
+create table box_chats (
+	id serial primary key,
+	box_name varchar(50) not null,
+	is_direct boolean default true,
+	create_date timestamp default current_timestamp
+);
+
+create table messages(
+	id serial primary key,
+	user_id int references user_accounts(id),
+	box_id int references box_chats(id),
+	content text not null,
+	create_date timestamp default current_timestamp
+);
+
+create table reports(
+	id serial primary key,
+	reported_id int references user_accounts(id),
+	reporter_id int references user_accounts(id),
+	create_date timestamp default current_timestamp
+);
+
+create table friendships(
+	request_id int references user_accounts(id),
+	accept_id int references user_accounts(id),
+	is_accepted boolean default false,
+	primary key(request_id, accept_id)
+);
+
+create table block_lists(
+	id serial primary key,
+	user_id int references user_accounts(id),
+	block_id int references user_accounts(id)
+);
+
+create table box_chat_members(
+
+	box_id int references box_chats(id),
+	user_id int references user_accounts(id),
+	is_admin boolean default false ,
+	create_date timestamp default current_timestamp,
+	primary key(box_id, user_id)
+);
+
+create table user_activity_logs(
+	user_id int references user_accounts(id),
+	session_start timestamp default current_timestamp,
+	session_end timestamp,
+	primary key (user_id, session_start)
+);
