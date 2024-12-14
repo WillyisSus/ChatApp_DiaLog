f05a8f22135fef32481c61002ebeda28f82a17c4
ibhnim4002 <88875055+ibhnim4002@users.noreply.github.com>
2024-12-14 09:11:58 +0700

    change regex

--- src/main/java/application/chatapp_dialog/security/UserRegistrationValidator.java
+++ src/main/java/application/chatapp_dialog/security/UserRegistrationValidator.java
@@ -4,26 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationValidator {
-    private static final String regexPassword = "^(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%]).+$";
-    private static final String regexEmail = "^([\\w\\-\\.]+)[a-zA-Z0-9]+@([\\w\\-]+\\.)+([a-zA-Z]{2,})$";
-    private static final String regexUsername = "^(?!admin)[a-zA-Z0-9]*$";
-    public static boolean validatePassword(String password){
-        if (password.trim().length() < 8){
-            return false;
-        }
-        System.out.println(password);
-        Pattern pattern = Pattern.compile(regexPassword, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(password);
-        return  matcher.find();
-    }
-
-    public static boolean validateEmail(String email){
-        Pattern pattern = Pattern.compile(regexEmail, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(email);
-        System.out.println(email);
-
-        return  matcher.find();
-    }
+    private static final String regexUsername = "^(?!admin)[a-zA-Z0-9]{6,32}$";
+    private static final String regexEmail = "^(?!.*[\\.]{2,})(?!.*\\.@)[\\w\\-][\\w\\-\\.]{0,63}@((?!\\-)[a-zA-Z0-9\\-]{0,62}[a-zA-Z0-9][\\.]){0,3}((?=.*[a-zA-z\\-])(?!\\-)[a-zA-Z0-9\\-]{0,62}[a-zA-Z0-9])$";
+    private static final String regexPassword = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%\\.])[a-zA-Z0-9!@#$%\\.]{8,32}$";
 
     public static boolean validateUsername(String username){
         Pattern pattern = Pattern.compile(regexUsername, Pattern.CASE_INSENSITIVE);
@@ -31,17 +14,30 @@ public class UserRegistrationValidator {
         Matcher matcher = pattern.matcher(username);
         return  matcher.find();
     }
-
-    public static String getRegexEmail() {
-        return regexEmail;
+    public static boolean validateEmail(String email){
+        Pattern pattern = Pattern.compile(regexEmail, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        System.out.println(email);
+        return  matcher.find();
     }
-
-    public static String getRegexPassword() {
-        return regexPassword;
+    public static boolean validatePassword(String password){
+        System.out.println(password);
+        Pattern pattern = Pattern.compile(regexPassword, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(password);
+        return  matcher.find();
     }
 
     public static String getRegexUsername() {
         return regexUsername;
     }
+    public static String getRegexPassword() {
+        return regexPassword;
+    }
+    public static String getRegexEmail() {
+        return regexEmail;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(validateEmail("-___@1-2"));
+    }
 }
